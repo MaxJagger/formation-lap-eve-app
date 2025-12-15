054197581fccb0b6fd20def0d09be2d102ad95e7
Saerochan <84173944+MaxJagger@users.noreply.github.com>
2025-12-15 10:54:46 +0900

    Update app.py

--- src/app.py
+++ src/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello, Kubernetes from Docker!\n", 200
+    return "Hello, Kubernetes from Docker!!\n", 200
 
 @app.route("/health")
 def health():
