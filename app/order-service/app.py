f91df66c8d8032a69ddf310382c346082f53b352
root <root@maxjagger.localdomain>
2025-12-28 20:28:25 +0900

    CI test

--- app/order-service/app.py
+++ app/order-service/app.py
@@ -12,7 +12,8 @@ def get_orders():
         "service": "order-service",
         "orders": [
             {"order_id": 101, "item": "laptop"},
-            {"order_id": 102, "item": "keyboard"}
+            {"order_id": 102, "item": "keyboard"},
+            {"order_id": 103, "item": "kup"}
         ]
     }
 
